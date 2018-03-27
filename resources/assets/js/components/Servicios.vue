e0ce544b67f149a9ed224f1caa757930414abdea
jhfredy <jhfredy84@hotmail.com>
2018-03-27 16:59:29 -0500

    merge

---
+++ resources/assets/js/components/Servicios.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row align-items-center">
+    <div class="col-md-3" v-for="servicio in servicios" :key="servicio.id">
+      <div class="card my-2">
+        <div class="card-body text-center">
+          <i class="fa fa-4x fa-globe"></i>
+          <h4>{{ servicio.nombre }}</h4>
+          <b>${{ servicio.costo | currency }}</b>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary btn-block" @click="$emit('selected', servicio)">
+            Contratar
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { servicios: [] };
+  },
+  created() {
+    axios.get("/api/servicio").then(res => (this.servicios = res.data));
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ resources/assets/js/components/inputs/FormInput.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="form-group" :class="state">
+		<label :for="name">{{ label }}</label>
+		<input :type="type" class="form-control"
+			:name="name"
+			:value="value" v-bind="$attrs" @input="input">
+		<div class="form-control-feedback">{{ errorMessages[0] }}</div>
+	</div>
+</template>
+<script>
+import InputField from '../../mixins/InputField'
+export default {
+	mixins: [InputField],
+	props: {
+		label: String
+	}
+}
+</script>
