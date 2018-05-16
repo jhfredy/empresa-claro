1587086fffeea14f84458b7eb91002a165960803
Stevenson Marquez <stevensonxmarquez@gmail.com>
2018-05-16 16:30:53 -0500

    Logos en Dash

--- resources/views/layouts/dash/nav.blade.php
+++ resources/views/layouts/dash/nav.blade.php
@@ -1,5 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Claro</a>
+    <a class="navbar-brand" href="{{ route('home') }}">
+      <img src="{{asset('img/logo-claro.svg')}}" class="img-responsive" style="width:5em">
+    </a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
